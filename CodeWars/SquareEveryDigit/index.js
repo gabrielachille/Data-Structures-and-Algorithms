f2aa76ed78a2318dfbe4b8ad979d877bd9185c65
Gabriel Achille <gabriel.achille13@gmail.com>
2026-07-08 22:41:25 -0400

    made changes

--- CodeWars/SquareEveryDigit/index.js
+++ CodeWars/SquareEveryDigit/index.js
@@ -1,13 +1,13 @@
 //separate every digit, square them, and then put them back together
 //and return it as a number
 
-function squareDigits(num) {
-  num = num
-    .toString()
-    .split("")
-    .map((x) => x * x)
-    .join("");
-  return +num;
-}
+// function squareDigits(num) {
+//   num = num
+//     .toString()
+//     .split("")
+//     .map((x) => x * x)
+//     .join("");
+//   return +num;
+// }
 
 console.log(squareDigits(345));
