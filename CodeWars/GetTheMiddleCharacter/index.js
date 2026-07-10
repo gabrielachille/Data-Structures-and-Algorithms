b80be4e3c58019f17b2462583397b9a55c312b47
Gabriel Achille <gabriel.achille13@gmail.com>
2026-07-09 23:59:29 -0400

    made changes

--- CodeWars/GetTheMiddleCharacter/index.js
+++ CodeWars/GetTheMiddleCharacter/index.js
@@ -1,10 +1,10 @@
 //return middle character/s of a word
 
-// function getMiddle(s) {
-//   return s.length % 2 == 1
-//     ? s[Math.floor(s.length / 2)]
-//     : s[s.length / 2 - 1] + s[s.length / 2];
-// }
+function getMiddle(s) {
+  return s.length % 2 == 1
+    ? s[Math.floor(s.length / 2)]
+    : s[s.length / 2 - 1] + s[s.length / 2];
+}
 
 console.log(getMiddle("riddle"));
 
