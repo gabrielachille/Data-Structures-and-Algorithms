60068e42648331b41d64ffd24189acc63d0cc5b8
Gabriel Achille <gabriel.achille13@gmail.com>
2026-07-12 23:12:36 -0400

    made changes

--- CodeWars/MultiplesOf3Or5/index.js
+++ CodeWars/MultiplesOf3Or5/index.js
@@ -2,16 +2,16 @@
 //3 and 5 in a given number
 //6 kyu
 
-function solution(number) {
-  let sum = 0;
+// function solution(number) {
+//   let sum = 0;
 
-  for (let i = 0; i < number + 1; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
-    }
-  }
-  return sum;
-}
+//   for (let i = 0; i < number + 1; i++) {
+//     if (i % 3 === 0 || i % 5 === 0) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// }
 
 console.log(solution(10));
 console.log(solution(23));
