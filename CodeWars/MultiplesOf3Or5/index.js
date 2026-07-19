c391219cea4980ab9d0b7bb3048a656a50851985
Gabriel Achille <gabriel.achille13@gmail.com>
2026-07-18 21:18:18 -0400

    made changes

--- CodeWars/MultiplesOf3Or5/index.js
+++ CodeWars/MultiplesOf3Or5/index.js
@@ -1,17 +1,17 @@
 //Return the sum of every number thats divisible by
-//3 and 5 in a given number
-//6 kyu
+3 and 5 in a given number
+6 kyu
 
-// function solution(number) {
-//   let sum = 0;
+function solution(number) {
+  let sum = 0;
 
-//   for (let i = 0; i < number + 1; i++) {
-//     if (i % 3 === 0 || i % 5 === 0) {
-//       sum += i;
-//     }
-//   }
-//   return sum;
-// }
+  for (let i = 0; i < number + 1; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
 
 console.log(solution(10));
 console.log(solution(23));
