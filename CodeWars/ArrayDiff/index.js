596738163bf6d8a4ba8415771c7191b57e17a1b6
Gabriel Achille <gabriel.achille13@gmail.com>
2026-09-04 08:58:40 -0400

    made changes

--- CodeWars/ArrayDiff/index.js
+++ CodeWars/ArrayDiff/index.js
@@ -2,9 +2,9 @@
 //from the first array
 //6kyu
 
-// function arrayDiff(a, b) {
-//   return a.filter((x) => !b.includes(x));
-// }
+function arrayDiff(a, b) {
+  return a.filter((x) => !b.includes(x));
+}
 
 console.log(arrayDiff([1, 2], [2]));
 console.log(arrayDiff([1, 2, 2, 2, 3], [2]));
